6978c2f4a43c648d4671cbdf6ef175db0579be17
Mohammad Sohail Qureshi <62835743+sohqureshi@users.noreply.github.com>
2026-04-25 02:08:17 +0530

    Update compact.ts

--- src/core/compact.ts
+++ src/core/compact.ts
@@ -1,10 +1,14 @@
 export function compact(data: any): string {
+  if (!data) return "";
+
   if (Array.isArray(data)) {
-    return data.map(d => Object.values(d).join(",")).join(";");
+    return data.map(item => compact(item)).join(";");
   }
 
-  if (data && typeof data === "object") {
-    return Object.values(data).join(",");
+  if (typeof data === "object") {
+    return Object.entries(data)
+      .map(([k, v]) => `${k}:${compact(v)}`)
+      .join(",");
   }
 
   return String(data);
